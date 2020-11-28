9e44f3ec0bdd5039e00c822fd98817a75bbd2a24
ilakshay14 <ihybrid11@gmail.com>
2020-11-29 00:39:36 +0530

    initial commit

--- server.js
+++ server.js
@@ -9,5 +9,4 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-//app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`App listening on port ${port}!`))
